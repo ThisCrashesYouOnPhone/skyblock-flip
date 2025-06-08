23b36e041ee1e70a0a850d11dd3b34a4849f601e
a a <raiexists@gmail.com>
2025-06-08 16:43:10 -0600

    hi

--- calculator.py
+++ calculator.py
@@ -3,11 +3,20 @@ import json
 # Hypixel Skyblock Auction House API Example
 
 api_key = "39244af7-0918-45b1-8920-c05bdd7ef1a7"
-api_url = "https://api.hypixel.net/v2/skyblock/auctions"
+api_url = "https://api.hypixel.net/v2/skyblock/auctions?page=2"
 
-data = requests.get(api_url).json()
+headers = {
+    "API-Key": api_key
+}
+all_auctions = []
+for page_num in range(1,5):
+    api_url = f"https://api.hypixel.net/v2/skyblock/auctions?page={page_num}"
+    print(f"Fetching data from: {api_url}")
+    x = requests.get(api_url, headers=headers)
+    all_auctions.append(x.json())
+    # Fetching the data from the API
 
 with open('auctions.json', 'w') as f:
-    json.dump(data, f) 
+    json.dump(all_auctions, f) 
 
-print(data)
+print(all_auctions)
